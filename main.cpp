8d8133058b75488c87a1d8e5d2dc0edb601a8d28
Дмитрий <dmitklgjob@gmail.com>
2024-08-24 15:32:02 +0300

    add c file

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+// g++ main.cpp funcTypesEquation.cpp debug.cpp release.cpp SolveEquation.cpp -o out
+
+/**
+        \brief Шаблонная функция извлечения корня из объекта
+
+        \tparam T - любой тип, из которого можно извлечь корень
+
+        \param [in] a Объект для извлечения корня
+
+        \return Новый объект типа *T*, равный корню *a*.
+
+
+        \code{cpp}
+        Math m;
+
+        double d = m.Sqrt(2.542);
+        \endcode
+
+        \warning Не принимает отрицательные значения
+    */
+int main(int argc, char *argv[])
+{
+    printf("ffff");
+}
